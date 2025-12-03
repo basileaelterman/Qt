c76d1d1f98635f900b4678ad424114239383e132
basileaelterman <basile.aelterman@email.com>
2025-12-03 20:48:29 +0100

    Introduction to Qt Widgets - Hello World.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow window;
+    QPushButton button("Hello, world!");
+    button.show();
     window.show();
     return app.exec();
 }
